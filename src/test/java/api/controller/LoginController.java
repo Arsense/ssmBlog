b4773092b5f3f445b3ec76619f2e6cfa4ec659a6
Arsense <466217395@qq.com>
2017-12-21 09:20:50 +0800

    update

--- src/test/java/api/controller/LoginController.java
+++ src/test/java/api/controller/LoginController.java
@@ -2,7 +2,6 @@ package api.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 

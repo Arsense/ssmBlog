104d25ed91680592ce7c04f029b225177e7e129f
Arsense <466217395@qq.com>
2017-12-27 11:06:58 +0800

    link ok

--- src/main/java/com/we/weblog/controller/LoginController.java
+++ src/main/java/com/we/weblog/controller/LoginController.java
@@ -31,7 +31,7 @@ public class LoginController {
      * @throws IOException
      */
 //    @PostMapping("/login")
-    @GetMapping("/admin")
+    @PostMapping("/adminer")
     public void checkLogin(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String username = (String) request.getParameter("username");
         String password = request.getParameter("password");

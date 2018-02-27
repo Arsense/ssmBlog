fd6a1f46a4318a194f964cf0c0de439fdc41b3f7
Arsense <466217395@qq.com>
2018-02-27 20:10:06 +0800

    更新小部分

--- src/main/java/com/we/weblog/controller/admin/PageController.java
+++ src/main/java/com/we/weblog/controller/admin/PageController.java
@@ -2,6 +2,7 @@ package com.we.weblog.controller.admin;
 
 
 import com.we.weblog.domain.Context;
+import com.we.weblog.domain.modal.Types;
 import com.we.weblog.service.ContextService;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Controller
@@ -29,34 +31,34 @@ public class PageController {
 
 
 
-    @GetMapping("/get")
+    @GetMapping("/publish")
     @ResponseBody
-    public void index(){
+    public void createPages(Context context) throws SQLException {
 
-         List<Context> contexts = contextService.getArticlePages();
-
-
-    }
-
-
-    @GetMapping("/edit")
-    public void editPage(@Param("id")int id){
-
+        context.setPublish(Types.PAGE);
+        contextService.addBlog(context);
 
     }
 
-    @GetMapping("/add")
-    public void addPage(@Param("id")int id){
+
+    @GetMapping("/modify")
+    @ResponseBody
+    public void modifyPages(Context context){
+            int a = 3;
+
 
 
     }
 
 
     @GetMapping("/delete")
-    public void deletePage(@Param("id")int id){
+    @ResponseBody
+    public void deletePages(int id){
 
-
+        contextService.deleteBlogById(id);
     }
+
+
 
 
 

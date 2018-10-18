38581be26e6681028948136db57e7482b1d764da
Arsense <466217395@qq.com>
2018-10-18 16:43:02 +0800

    重构优化

--- src/main/java/com/we/weblog/domain/modal/LogActions.java
+++ src/main/java/com/we/weblog/domain/modal/LogActions.java
@@ -2,13 +2,13 @@ package com.we.weblog.domain.modal;
 
 public interface LogActions {
 
-    String LOGIN="登陆后台";
+    String LOGIN = "登陆后台";
 
-    String ADD_BLOG="添加新博客";
+    String ADD_BLOG = "添加新博客";
 
     String ADD_PAGES = "添加新的页面";
 
-    String DELETE_BLOG="删除博客";
+    String DELETE_BLOG = "删除博客";
 
-    String LOGOUT="退出登录";
+    String LOGOUT = "退出登录";
 }

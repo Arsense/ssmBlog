563bdaed93941cee4ea7e029283c662fd3de4786
Arsense <466217395@qq.com>
2018-10-23 15:42:23 +0800

    配置bug优化

--- src/main/resources/ui-model/app_info.s
+++ src/main/resources/ui-model/app_info.s
@@ -1,3 +1,3 @@
 len_val1.0
 appName,indexUrl,profileUrl,signOutUrl,logoName,logoShortName,copyright,version,
-5:Clay1,11:/index.html,0:,0:,4:Clay,4:Clay,4:Clay,0:,
+4:Clay,11:/index.html,0:,0:,4:Clay,4:Clay,0:,0:,
